d612d168418e281eb757c355aaf52febb18cd71b
Ramiro Arthur <ramiro.ab.martins@gmail.com>
2023-02-22 09:38:22 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,50 +7,49 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Resevation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.print("Room number: ");
-		int roomNumber = sc.nextInt();
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
-		
-		//Ainda temos essa validação feita no programa principal, que é na hora de INSTANCIAR o objeto!
-		//Ainda não vamos tirar essa validação daqui pq ela tenque ser uma validação no CONSTRUTOR!! ja que é na INSTANCIAÇÃO do objeto
-		//Porém, o CONSTRUTOR não tem como eu colocar ele para retornar um String!
-		if (!checkOut.after(checkIn)) { 
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}
-		else {
+		try {
+			System.out.print("Room number: ");
+			int roomNumber = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			
 			Resevation reservation = new Resevation(roomNumber, checkIn, checkOut); //Instanciei minha reserva
+				
 			System.out.println("Reservation: " + reservation);
-		
+			
 			System.out.println();
-		
+			
 			System.out.println("Enter data to update the reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			
-			String error = reservation.updateDates(checkIn, checkOut); 
-			//Agora a função reservarion() do objt reservation vai retornar uma String, que vamos guardar!	
-			if (error != null) {
-				System.out.println("Error in reservations: " + error);
-			}
-			else {
-				System.out.println("Reservation: " + reservation);
-			}
+				
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
 		}
-	sc.close();
+		catch (ParseException e) {
+			System.out.println("Invalide date format");
+		}
+		catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		sc.close();
 	}
 }
 
--- src/model/entities/Resevation.java
+++ src/model/entities/Resevation.java
@@ -1,26 +1,25 @@
 package model.entities;	
 
-import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;		
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-//--Lógica de Validação DELEGADA para a classe [Reservartion] "tratamento da excessão"--
+import model.exceptions.DomainException;
+
 public class Resevation {
 
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");  //Para formatar um tipo Date!
-	//statico para que não seja instanciado um novo objeto SimpleDateFormat para cada Objeto Reservation que minha aplicação tiver!
-	//Vou precisar de apenas 1, por isso coloquei como static!
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	private Integer roomNumber;
-	private Date checkIn;		//Obs: Data trabalha com milissegundos
+	private Date checkIn;	
 	private Date checkOut;
-	
-	//Sem nos preocuparmos ainda com excessões!
 	
 	public Resevation() {
 	}
 
 	public Resevation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn))
+			throw new DomainException("Check-out date must be after check-in date"); 
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -41,31 +40,22 @@ public class Resevation {
 	public Date getCheckout() {
 		return checkOut;
 	}
-
-	//Não temos setCheckout e o setCheckin pois não vou dexar que as datas sejam alteradas arbitariamente!
-	//Temos um metodo para isso  updateDates()
 	
-	public long duration() { //Usaremos um macete para diferença entre datas que me retorna um valor long
+	public long duration() {
 		
-		long diff = checkOut.getTime() - checkIn.getTime();//Diferença entre as duas Datas em milissegundos!
-		
-		//Agora como converto esse milissegundos para dias??? 
-		//Vou usar uma Classe do JAVA chamada TimeUnit -- Na verdade ela é um tipo ENUMERADO complexo que tem operações! Ñ classe
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); //Isso converte o valor da var: diff que estava em milisseconds para Dias!	
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
-		Date now = new Date(); //Objeto com data atual!
-		if (checkIn.before(now) || checkOut.before(now)) { //Se a data checkin for antes do agora ou a data de chekout tmb
-			return "Reservation dates for update must be future dates"; //Retorna essa String
-		}
-		if (!checkOut.after(checkIn)) { //Se a data de checkout nao é posterior a data de chekin
-			return "Check-out date must be after check-in date"; //Retorna essa String
-		}
+	public void updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now))
+			throw new DomainException("Reservation dates for update must be future dates");
+		if (!checkOut.after(checkIn))
+			throw new DomainException("Check-out date must be after check-in date"); 
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null; //Para indicar que não teve nenhum erro, mando retornar NULL
-		//null é o critério para dizer que minha operação não deu nenhum erro!
 	}
 	
 	@Override
